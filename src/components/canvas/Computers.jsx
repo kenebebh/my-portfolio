dd7be5e83099e6ba922feb7964035941e0eb55cb
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2023-04-08 03:23:21 +0100

    finished the entire hero component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
@@ -18,8 +18,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.7}
-        position={[0, -3, 0]}
+        scale={isMobile ? 0.5 : 0.7}
+        position={isMobile ? [0, -2.5, 0] : [0, -3, 0]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -27,6 +27,23 @@ const Computers = () => {
 };
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -42,7 +59,7 @@ const ComputerCanvas = () => {
           autoRotate={true}
           autoRotateSpeed={5}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
