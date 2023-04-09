7ee63f78e55aeb85c678cbf4a8e56a67983d0f68
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2023-04-09 16:44:14 +0100

    started the projects section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,23 +1,35 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
 // import { Text3D } from "@react-three/drei";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div
-          className="w-32 h-32 flex flex-col items-center"
-          key={technology.name}
-        >
-          <BallCanvas icon={technology.icon} />
-          {/* <Text3D smooth={1} lineHeight={0.5} letterSpacing={-0.025}>
+    <div className="flex flex-col">
+      <div className="mb-12">
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>My Stack</p>
+          <h2 className={styles.sectionHeadText}>Technologies.</h2>
+        </motion.div>
+      </div>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div
+            className="w-32 h-32 flex flex-col items-center"
+            key={technology.name}
+          >
+            <BallCanvas icon={technology.icon} />
+            {/* <Text3D smooth={1} lineHeight={0.5} letterSpacing={-0.025}>
             {technology.name}
           </Text3D> */}
-          <p>{technology.name}</p>
-        </div>
-      ))}
+            <p>{technology.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
