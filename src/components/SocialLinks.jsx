3846cc9efb3a0711694159b4f43f44fc256d742d
kenebebh <kenebebhbanigo@gmail.com>
2023-04-17 19:05:25 +0100

    added all my contact links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -7,27 +7,43 @@ const SocialLinks = () => {
   const style = { color: "#915eff", fontSize: "2.5rem" };
 
   return (
-    <div className="fixed bottom-12 right-0 w-16 h-56 flex flex-col justify-evenly">
+    <div className="fixed bottom-12 right-0 w-12 h-56 flex flex-col justify-evenly sm:mr-0 md:mr-2">
       <IconContext.Provider value={{ className: "contactIcon" }}>
-        <a href="http://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.linkedin.com/in/kenebebh-banigo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaLinkedin style={style} />
         </a>
       </IconContext.Provider>
 
       <IconContext.Provider value={{ className: "contactIcon" }}>
-        <a href="http://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://twitter.com/BanigoKene"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaTwitter style={style} />
         </a>
       </IconContext.Provider>
 
       <IconContext.Provider value={{ className: "contactIcon" }}>
-        <a href="http://hashnode.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://bebh.hashnode.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <SiHashnode style={style} />
         </a>
       </IconContext.Provider>
 
       <IconContext.Provider value={{ className: "githubContactIcon" }}>
-        <a href="http://github.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/kenebebh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaGithub style={style} />
         </a>
       </IconContext.Provider>
